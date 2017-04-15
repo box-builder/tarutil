f2199a335073272e6ff0657f439e8618a552ace6
Erik Hollensbe <github@hollensbe.org>
2017-04-15 16:36:19 -0700

    tarutil_test.go: two new tests centered around UnpackTar
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- tarutil_test.go
+++ tarutil_test.go
@@ -1,6 +1,53 @@
 package tarutil
 
-import "testing"
+import (
+	"context"
+	"io"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	_ "crypto/sha256"
+
+	digest "github.com/opencontainers/go-digest"
+)
+
+const emptyDigest = digest.Digest("sha256:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855")
+
+func TestUntarTeeReader(t *testing.T) {
+	dir, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer os.RemoveAll(dir)
+	r := generateTar(25)
+	dgr := digest.SHA256.Digester()
+
+	tee := io.TeeReader(r, dgr.Hash())
+
+	if err := UnpackTar(context.Background(), tee, dir); err != nil {
+		t.Fatal(err)
+	}
+
+	if dgr.Digest() == emptyDigest {
+		t.Fatal("digest was empty")
+	}
+}
+
+func TestUntarReader(t *testing.T) {
+	dir, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
+	r := generateTar(25)
+
+	if err := UnpackTar(context.Background(), r, dir); err != nil {
+		t.Fatal(err)
+	}
+}
 
 func TestDirectoryExists(t *testing.T) {
 	ok, err := directoryExists("/tmp")
