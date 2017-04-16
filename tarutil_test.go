76f5bbeb4e00826a04a54fcc7898262cae316bbb
Erik Hollensbe <github@hollensbe.org>
2017-04-15 17:57:54 -0700

    use errors.Wrap everywhere
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- tarutil_test.go
+++ tarutil_test.go
@@ -10,6 +10,7 @@ import (
 	_ "crypto/sha256"
 
 	digest "github.com/opencontainers/go-digest"
+	"github.com/pkg/errors"
 )
 
 const emptyDigest = digest.Digest("sha256:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855")
@@ -111,7 +112,7 @@ func TestDirectoryExists(t *testing.T) {
 	}
 
 	ok, err = directoryExists("/dev/null")
-	if err != errPathIsNonDirectory {
+	if errors.Cause(err) != errPathIsNonDirectory {
 		t.Fatalf("expected error: %v", err)
 	}
 
