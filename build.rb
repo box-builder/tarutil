aa76f2d1a56becc733dfc4d371f8a00acb96102a
Erik Hollensbe <github@hollensbe.org>
2017-04-17 06:46:15 -0700

    .gitignore,build.rb,vendor.conf: use github.com/LK4D4/vndr
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- build.rb
+++ build.rb
@@ -7,7 +7,7 @@ path = "#{TARGET}/#{REPO}"
 
 copy ".", path
 workdir path
-run "go get -t -d ./..."
+run "set -e; if [ ! -d vendor ]; then go get github.com/LK4D4/vndr && vndr; fi"
 
 # this entrypoint clear works around a box 0.5.1 bug.
 set_exec entrypoint: [], cmd: ["/bin/sh", "-c", "go test -cover -v ./..."]
