bb8148b76eab922ca5c6f115e5705e0b1219e39c
Erik Hollensbe <github@hollensbe.org>
2017-04-17 11:07:21 -0700

    pack.go,pack_test.go: symlink/hard link handling
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- pack.go
+++ pack.go
@@ -6,13 +6,59 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"syscall"
+
+	"github.com/pkg/errors"
 )
+
+func getLink(source, p string, fi os.FileInfo, inodeTable map[uint64]string) (string, bool, error) {
+	if fi.Mode()&os.ModeSymlink == os.ModeSymlink {
+		follow, err := filepath.EvalSymlinks(p)
+		if err != nil {
+			return "", false, errors.Wrap(errInvalidSymlink, err.Error())
+		}
+
+		rel, err := filepath.Rel(source, follow)
+		if err != nil {
+			return "", false, errors.Wrap(errInvalidSymlink, err.Error())
+		}
+
+		return rel, false, nil
+	}
+
+	inode := fi.Sys().(*syscall.Stat_t).Ino
+	nlink := fi.Sys().(*syscall.Stat_t).Nlink
+
+	if nlink > 1 {
+		if _, ok := inodeTable[inode]; ok {
+			return inodeTable[inode], true, nil
+		}
+
+		rel, err := filepath.Rel(source, p)
+		if err != nil {
+			return "", false, errors.Wrap(errInvalidLink, err.Error())
+		}
+
+		inodeTable[inode] = rel
+		return "", false, nil
+	}
+
+	return "", false, nil
+}
 
 // Pack packs a tarball from the specified source, into the writer w. Returns
 // an error.
 func Pack(ctx context.Context, source string, w io.Writer) error {
+	inodeTable := map[uint64]string{}
+
 	tw := tar.NewWriter(w)
+	defer tw.Close()
+
 	err := filepath.Walk(source, func(p string, fi os.FileInfo, err error) error {
+		if p == source {
+			return nil
+		}
+
 		if err != nil {
 			return err
 		}
@@ -22,18 +68,31 @@ func Pack(ctx context.Context, source string, w io.Writer) error {
 			return err
 		}
 
-		header, err := tar.FileInfoHeader(fi, "")
+		linkName, hardLink, err := getLink(source, p, fi, inodeTable)
 		if err != nil {
 			return err
 		}
 
+		header, err := tar.FileInfoHeader(fi, linkName)
+		if err != nil {
+			return err
+		}
+
+		header.Linkname = linkName
 		header.Name = rel
+
+		if hardLink {
+			header.Typeflag = tar.TypeLink
+			header.Size = 0
+		} else if linkName != "" {
+			header.Typeflag = tar.TypeSymlink
+		}
 
 		if err := tw.WriteHeader(header); err != nil {
 			return err
 		}
 
-		if !fi.IsDir() {
+		if !fi.IsDir() && header.Linkname == "" {
 			abs, err := filepath.Abs(p)
 			if err != nil {
 				return err
@@ -46,12 +105,10 @@ func Pack(ctx context.Context, source string, w io.Writer) error {
 			if err != nil {
 				return err
 			}
-			return f.Close()
+			f.Close()
 		}
-
 		return nil
 	})
 
-	tw.Close()
 	return err
 }
--- pack_test.go
+++ pack_test.go
@@ -8,7 +8,11 @@ import (
 	"os"
 	"path"
 	"path/filepath"
+	"strings"
+	"syscall"
 	"testing"
+
+	"github.com/pkg/errors"
 )
 
 func generateWord(length int) string {
@@ -55,7 +59,17 @@ func generateFiles(num, filelen int) (string, []string, error) {
 
 		f.Close()
 
+		if err := os.Symlink(fullPath, fullPath+".symlink"); err != nil {
+			return "", nil, err
+		}
+
+		if err := os.Link(fullPath, fullPath+".lnk"); err != nil {
+			return "", nil, err
+		}
+
 		ret = append(ret, f.Name())
+		ret = append(ret, f.Name()+".symlink")
+		ret = append(ret, f.Name()+".lnk")
 	}
 
 	return basePath, ret, nil
@@ -70,9 +84,10 @@ func TestPack(t *testing.T) {
 
 	r, w := io.Pipe()
 
-	//tr := tar.NewReader(r)
 	go func() {
-		w.CloseWithError(Pack(context.Background(), packDir, w))
+		if err := Pack(context.Background(), packDir, w); err != nil {
+			panic(err) // t.Fatal is a little dodgy in goroutines
+		}
 	}()
 
 	unpackDir, err := ioutil.TempDir("", "")
@@ -86,25 +101,71 @@ func TestPack(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	var abort string
+	var count int
 
-	err = filepath.Walk(unpackDir, func(p string, fi os.FileInfo, err error) error {
-		abort = p
+	if err := filepath.Walk(unpackDir, walkUnpack(unpackDir, files, &count)); err != nil {
+		t.Fatalf("Aborted due to missing file: error: %v", err)
+	}
+
+	if len(files) != count {
+		t.Fatal("Did not walk all files for some reason...")
+	}
+}
+
+func walkUnpack(unpackDir string, files []string, count *int) func(string, os.FileInfo, error) error {
+	return func(p string, fi os.FileInfo, err error) error {
+		if p == unpackDir {
+			return nil
+		}
+
+		*count++
+		abort := p
 
 		for _, file := range files {
 			if path.Base(file) == path.Base(p) {
+				switch path.Ext(p) {
+				case ".symlink":
+					fi, err := os.Lstat(p)
+					if err != nil {
+						return err
+					}
+
+					if fi.Mode()&os.ModeSymlink != os.ModeSymlink {
+						return errors.Errorf("symlink %q was not a symlink", p)
+					}
+
+					pristine, err := filepath.EvalSymlinks(p)
+					if err != nil {
+						return err
+					}
+
+					if pristine != strings.TrimSuffix(p, ".symlink") {
+						return errors.Errorf("symlink %q did not link to regular file", p)
+					}
+				case ".lnk":
+					fi, err := os.Lstat(p)
+					if err != nil {
+						return err
+					}
+
+					pristine, err := os.Lstat(strings.TrimSuffix(p, ".lnk"))
+					if err != nil {
+						return err
+					}
+
+					if pristine.Sys().(*syscall.Stat_t).Ino != fi.Sys().(*syscall.Stat_t).Ino {
+						return errors.Errorf("%q is not a hard link to %q", p, pristine.Name())
+					}
+				}
+
 				abort = ""
 			}
 		}
 
 		if len(abort) != 0 {
-			return nil
+			return errors.Errorf("missed file %q", abort)
 		}
 
 		return nil
-	})
-
-	if err != nil || len(abort) != 0 {
-		t.Fatalf("Aborted due to missing file %q: error (if any): %v", abort, err)
 	}
 }
